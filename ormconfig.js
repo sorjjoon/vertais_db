b2bd54d81371be9ebd7c0e4409dfca8438407c4f
joona <joona.sorjonen97@gmail.com>
2022-02-09 23:11:54 +0200

    Recommit ormconfig

---
+++ ormconfig.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const connectionUrlParser = require("pg-connection-string");
+const config = connectionUrlParser.parse(process.env.DATABASE_URL);
+
+module.exports = [
+  {
+    name: "default",
+    type: "postgres",
+    host: config.host,
+    port: config.port,
+    username: config.user,
+    password: config.password,
+    database: config.database,
+    applicationName: "vertais, typeorm",
+    dropSchema: false,
+    synchronize: process.env.NODE_ENV !== "production",
+    entities: [path.join(__dirname, "dist", "entities", "*.js")],
+    migrationsTableName: "migrations",
+    migrations: [path.join(__dirname, "dist", "migrations", "*.js")],
+    cli: {
+      migrationsDir: [path.join(__dirname, "dist", "migrations", "*.js")],
+    },
+    ssl: { rejectUnauthorized: false, ssl: process.env.NODE_ENV === "production" },
+  },
+];
